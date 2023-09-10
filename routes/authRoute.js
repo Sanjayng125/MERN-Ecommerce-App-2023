0092ebe7c7dd6b08281a30902dc7f68b108910fb
Sanjayzng <106653066+Sanjayzng@users.noreply.github.com>
2023-09-10 08:58:08 +0530

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -8,6 +8,7 @@ import {
   getOrdersController,
   getAllOrdersController,
   orderStatusController,
+  getAllUsersController,
 } from "../controllers/authController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
@@ -53,5 +54,8 @@ router.put(
   isAdmin,
   orderStatusController
 );
+
+// get all users
+router.get("/all-users", requireSignIn, isAdmin, getAllUsersController);
 
 export default router;
